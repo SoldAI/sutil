Update of the TextVectorizer class, Update of the OneHotVectorizer, Fix of normalization issue in the DataSet class, implementation of the TextDAtaSet class

sutil/base/Dataset.py
@@ -96,7 +96,10 @@ class Dataset(object):
         for i in range(self.n):
             mu[i] = np.mean(self.X[:, i])
             sigma[i] = np.std(self.X[:, i])
-            X_norm[:, i] = (self.X[:, i] - mu[i]) / sigma[i]
+            if sigma[i] == 0:
+                X_norm[:, i] = (self.X[:, i] - mu[i])
+            else:
+                X_norm[:, i] = (self.X[:, i] - mu[i]) / sigma[i]
         self.X = X_norm
         self.mu = mu
         self.sigma = sigma
@@ -109,7 +112,10 @@ class Dataset(object):
         assert(self.isNormalized() == True, 'The normalization process has not been done')
         x_norm = example
         for i in range(len(x_norm)):
-            x_norm[:, i] = (self.x_norm[:, i] - self.mu[i]) / self.sigma[i]
+            if self.sigma[i] == 0:
+                x_norm[:, i] = (self.x_norm[:, i] - self.mu[i]) 
+            else:
+                x_norm[:, i] = (self.x_norm[:, i] - self.mu[i]) / self.sigma[i]
         return x_norm
 
     #Returns the test, validation and test datasets

sutil/text/TextDataset.py
@@ -8,20 +8,13 @@ import pandas as pd
 class TextDataset(Dataset):
 
     @classmethod
-    def fromDataFile(cls, filename, delimiter):
-        data = np.loadtxt(filename, delimiter=delimiter)
-        X = data[:, 0:-1]
-        y = data[:, -1]
-        return cls(X, y)
-
-    @classmethod
     def standard(cls, filename, delimiter):
         preprocessor = PreProcessor.standard()
         tokenizer = PhraseTokenizer()
         vectorizer = OneHotVectorizer({}, tokenizer)
         df = pd.read_csv(filename)
-        X = df.columns[0]
-        y = df.columns[-1]
+        X = df[df.columns[0]]
+        y = df[df.columns[-1]].values
         return cls(X, y, vectorizer, preprocessor)
 
     def __init__(self, X, y, vectorizer = None, preprocessor = None, **kwargs):
@@ -33,16 +26,15 @@ class TextDataset(Dataset):
             self.preprocessor = preprocessor 
         else: 
             self.preprocessor = PreProcessor.standard()
+        print(X)
         self.initialize(X, y, **kwargs)
 
     def initialize(self, texts, y, **kwargs):
         self.texts = texts
         processed = self.preprocessor.batchPreProcess(texts)
         self.vectorizer.initialize(processed)
-        self.vectorize(texts)
-        self.setData(self.X, y, kwargs)
-
-    def vectorize(self, texts):
-        self.X = self.vectorizer.textToMatrix(texts)
-        self.n = self.X.shape[1]
+        X = self.vectorizer.textToMatrix(texts)
+        print(X)
+        print(y)
+        self.setData(X, y, kwargs)
         self.X, self.mu, self.sigma = self.normalizeFeatures()

sutil/text/TextVectorizer.py
@@ -6,8 +6,13 @@ class TextVectorizer():
         self.dictionary = dictionary if dictionary else {}
         self.tokenizer = tokenizer
         self.entries = len(dictionary)
+        self.index = {}
+        for k, v in self.dictionary.items():
+            if not k in self.index.keys():
+                self.index[k] = len(self.index)
 
     def initialize(self, texts):
+        self.index = {}
         for t in texts:
             tokens = self.tokenizer(t)
             for token in tokens:
@@ -15,19 +20,20 @@ class TextVectorizer():
                     self.dictionary[token] += 1 
                 else:
                     self.dictionary[token] = 1
+                    self.index[token] = len(self.index)
 
     def encodePhrase(self, phrase):
         vector = np.zeros(len(self.dictionary))
         for t in self.tokenizer.getTokens(phrase):
             if t in self.dictionary.keys():
-                vector[self.dictionary[t]] = 1
+                vector[self.index[t]] = self.dictionary[t]
         return vector
 
     def decodeVector(self, vector):
-        inv_dict = {v: k for k, v in self.dictionary.items()}
+        inv_dict = {v: k for k, v in self.index.items()}
         out = ""
         for i in len(vector):
-            if vector[i] == 1:
+            if vector[i] != 0:
                 out += str(inv_dict[i]) + ","
         return out
 